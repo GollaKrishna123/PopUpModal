c5d927b658b849eefbe8795e43a6210ee5f8432d
GollaKrishna123 <gkrishna05348@gmail.com>
2023-05-24 17:07:00 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { Modal } from 'bootstrap';
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import PopUpModal from './components/PopUpModel';
 
-function App() {
+// Here we are using react-bootstrap package for modal instead of using bootstrap because for modal react bootstrap is better 
+
+const App = () => {
+  const [modalShow, setModalShow] = React.useState(false);
+  const [points,setPoints] = useState(0);
+  let totalPoints=100;
+  const incrementHandler = ()=>{
+    if(points<100)
+    setPoints(points+10);
+  }
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <Button variant="primary" onClick={() => setModalShow(true)}>
+        Launch vertically centered modal
+      </Button> */}
+
+      <PopUpModal
+        show={points===totalPoints}
+        onHide={() => setModalShow(false)}
+      />
+      <h1>{points}/{totalPoints}</h1>
+      <button onClick={incrementHandler}>Increase</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
